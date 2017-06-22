e50fd3ad560727dd068bb46571910ae204fdeb64
Matt Carter <m@ttcarter.com>
2017-06-22 10:18:58 +1000

    Initial commit

---
+++ app.js
@@ -0,0 +1,104 @@
+var _ = require('lodash');
+var async = require('async-chainable');
+var colors = require('chalk');
+var electron = require('electron');
+var program = require('commander');
+
+// Global objects {{{
+var app;
+var win;
+// }}}
+
+// Process command line args {{{
+program
+	.version(require('./package.json').version)
+	.option('-v, --verbose', 'Be verbose. Specify multiple times for increasing verbosity', function(i, v) { return v + 1 }, 0)
+	.option('--no-color', 'Disable colors')
+	.parse(process.env.PROGRAM_ARGS ? JSON.parse(process.env.PROGRAM_ARGS) : '')
+// }}}
+
+async()
+	// Setup main process {{{
+	.then(function(next) {
+		process.title = 'dedupe-ui';
+		next();
+	})
+	// }}}
+	// Setup browser app {{{
+	.then(function(next) {
+		if (program.verbose >= 3) console.log(colors.blue('[DeDupe-UI]'), 'Setting up Electron instance');
+
+		app = electron.app
+			.once('window-all-closed', function() {
+				if (program.verbose >= 2) console.log(colors.blue('[DeDupe-UI]'), 'All windows closed');
+				if (process.platform != 'darwin') app.quit(); // Kill everything if we're on Darwin
+			})
+			.once('error', next);
+
+		// We have to loop until electron is ready - see https://github.com/electron/electron/issues/1726
+		var checkReady = function() {
+			if (app.isReady()) {
+				if (program.verbose >= 2) console.log(colors.blue('[DeDupe-UI]'), 'Electron app ready');
+				next();
+			} else {
+				setTimeout(checkReady, 10);
+			}
+		};
+		checkReady();
+	})
+	// }}}
+	// Setup electron page {{{
+	.then(function(next) {
+		// Create the browser window.
+		if (program.verbose >= 3) console.log(colors.blue('[DeDupe-UI]'), 'Creating Electron window');
+		win = new electron.BrowserWindow({
+			width: 700,
+			height: 500,
+			frame: true,
+			title: 'DeDupe-UI',
+			show: false,
+			resizable: true,
+			center: true,
+		});
+
+		// Disable menu
+		win.setMenu(null);
+
+		// Prevent title changes
+		win.on('page-title-updated', e => e.preventDefault())
+
+		// win.loadURL('file://' + tempFile);
+		win.loadURL('http://google.com');
+
+		win.webContents.once('dom-ready', function() {
+			if (program.verbose >= 3) console.log(colors.blue('[DeDupe-UI]'), 'Electron DOM ready');
+			win.show();
+			return next();
+		});
+	})
+	// }}}
+	// Wait for window to terminate {{{
+	.then(function(next) {
+		win.on('closed', function() {
+			next();
+		});
+	})
+	// }}}
+	// End {{{
+	.end(function(err) {
+		// Clean up references {{{
+		if (app) app.quit();
+		win = null; // Remove reference and probably terminate the program
+		// }}}
+
+		// Handle exit state {{{
+		if (err) {
+			console.log(colors.blue('[DeDupe-UI]'), colors.red('ERR'), err.toString());
+			process.exit(1);
+		} else {
+			if (program.verbose) console.log(colors.blue('[DeDupe-UI]'), 'Exit');
+			process.exit(0);
+		}
+		// }}}
+	});
+	// }}}
---
+++ start.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+/**
+* Helper application to bundle up the command line into an Electron application
+* This program really just relaunches the electron shell encoding the process.argv structure into the process.env.CONKIE_ARGS as a JSON array
+*/
+
+var childProcess = require('child_process');
+var electron = require('electron-prebuilt');
+
+// Determine whether to detach from parent if the --background || -b flag is present as an arg
+var detach = process.argv.some(function(i) {
+	return (
+		i == '--background' || // Is the longhand flag
+		/^-(?!-).*b.*$/.test(i) // Is contained in a shorthand flag
+	);
+});
+
+// Change to this directory to keep Electron's require() statements happy
+process.chdir(__dirname);
+
+childProcess.spawn(electron, [
+	'--enable-transparent-visuals',
+	'--disable-gpu',
+	__dirname + '/app.js',
+], {
+	stdio: 'inherit',
+	detached: detach,
+	env: function() { // Inherit this envrionment but glue CONKIE_ARGS to the object
+		var env = process.env;
+		env.PROGRAM_ARGS = JSON.stringify(process.argv);
+		return env;
+	}(),
+});
+
+if (detach) process.exit(0);
