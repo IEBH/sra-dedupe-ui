42d302724dfed5de3391eb1a1f4c6f0c77f1998a
Matt Carter <m@ttcarter.com>
2018-08-20 09:17:26 +1000

    Upgraded to Webpack 4

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,12 @@ var _ = require('lodash');
 var gulp = require('gulp');
 var nodemon = require('nodemon');
 var os = require('os');
+var runSequence = require('run-sequence');
 var watch = require('gulp-watch');
 var webpack = require('webpack');
 var webpackStream = require('webpack-stream');
+
+var isDev = true;
 
 // Webpack options {{{
 var webpackOptions = {
@@ -12,21 +15,40 @@ var webpackOptions = {
 		filename: 'app.js',
 	},
 	module: {
-		loaders: [
-			{test: /\.css$/, loader: 'style!css'},
+		rules: [
+			// CSS {{{
 			{
-				test: /\.(jpe?g|png|gif|svg)$/i,
-				loaders: [
-					'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
-					'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
-				],
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: isDev,
+						},
+					},
+				]
 			},
-			{test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/, loader: 'url-loader'},
+			// }}}
+			// Images {{{
+			{
+				test: /\.(jpe?g|png|gif)$/,
+				loader: 'file-loader',
+			},
+			// }}}
+			// Fonts {{{
+			{
+				test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
+				loader: 'url-loader',
+			},
+			// }}}
+			// JS files {{{
 			{
 				test: /\.js$/,
-				exclude: /node_modules/,
 				loader: 'babel-loader',
-				query: {
+				exclude: /node_modules/,
+				options: {
+					compact: true,
 					presets: ['es2015'],
 					plugins: [
 						'babel-plugin-transform-es2015-template-literals',
@@ -34,20 +56,17 @@ var webpackOptions = {
 					],
 				},
 			},
+			// }}}
 		],
 	},
-	resolve: {
-		root: __dirname,
-		extensions: ['', '.js', '.css'],
-	},
 	plugins: [
+		// Inject jQuery {{{
 		new webpack.ProvidePlugin({
 			$: 'jquery',
 			jQuery: 'jquery',
 			'window.jQuery': 'jquery',
 		}),
-		// FIXME: Remove comment below
-		// new webpack.optimize.UglifyJsPlugin(),
+		// }}}
 	],
 	externals: [
 		// Workaround to stop Webpack trying to resolve the meta-object 'electron' which is injected at runtime {{{
@@ -66,7 +85,7 @@ var webpackOptions = {
 gulp.task('default', ['serve']);
 gulp.task('build', ['build:app']);
 
-// build:app - build the front end app {{{
+// build:app - build the front end app
 gulp.task('build:app', ['build:app:resources', 'build:app:webpack']);
 
 gulp.task('build:app:resources', ()=>
@@ -81,9 +100,10 @@ gulp.task('build:app:webpack', ()=>
 		.pipe(webpackStream(webpackOptions))
 		.pipe(gulp.dest('build'))
 );
-// }}}
 
-// serve - use nodemon to constantly restart the app during development {{{
+
+// serve - use nodemon to constantly restart the app during development
+// NOTE: To run with full debugging use `gulp serve serve:debug`
 gulp.task('serve', ['build'], function(done) {
 	var runCount = 0;
 	var monitor = nodemon({
@@ -101,4 +121,15 @@ gulp.task('serve', ['build'], function(done) {
 		gulp.start('build:app');
 	});
 });
-// }}}
+
+
+// Run 'serve' with a debug UI
+// This can be combined with other 'serve:*' flags
+gulp.task('serve:debug', done => runSequence('serve:set:debug', 'serve', done));
+gulp.task('serve:set:debug', ()=> process.env.DEBUG = 'dedupe-ui');
+
+
+// Run 'serve' with a filename
+// This can be combined with other 'serve:*' flags
+gulp.task('serve:file', done => runSequence('serve:set:file', 'serve', done));
+gulp.task('serve:set:file', ()=> process.env.DEDUPE_FILE = 'test/data/dupes-obvious.json');
