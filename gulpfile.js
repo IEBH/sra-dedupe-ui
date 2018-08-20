2203f5a42e373697267fb891de86f381e11387c9
Matt Carter <m@ttcarter.com>
2018-08-20 12:11:24 +1000

    Moved compile steps from package.json to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,17 @@ var gulp = require('gulp');
 var nodemon = require('nodemon');
 var os = require('os');
 var runSequence = require('run-sequence');
+var spawn = require('child_process').spawn;
 var watch = require('gulp-watch');
 var webpack = require('webpack');
 var webpackStream = require('webpack-stream');
 
-
+// Redirectors
 gulp.task('default', ['serve']);
 gulp.task('build', ['build:app']);
 
-// build:app - build the front end app
+
+// build:app - build the front end app {{{
 gulp.task('build:app', ['build:app:resources', 'build:app:webpack']);
 
 gulp.task('build:app:resources', ()=>
@@ -26,9 +28,9 @@ gulp.task('build:app:webpack', ()=>
 		.pipe(webpackStream(require('./webpack.config.js')))
 		.pipe(gulp.dest('build'))
 );
+// }}}
 
-
-// serve - use nodemon to constantly restart the app during development
+// serve - use nodemon to constantly restart the app during development {{{
 // NOTE: To run with full debugging use `gulp serve serve:debug`
 gulp.task('serve', ['build'], function(done) {
 	var runCount = 0;
@@ -59,3 +61,18 @@ gulp.task('serve:set:debug', ()=> process.env.DEBUG = 'dedupe-ui');
 // This can be combined with other 'serve:*' flags
 gulp.task('serve:file', done => runSequence('serve:set:file', 'serve', done));
 gulp.task('serve:set:file', ()=> process.env.DEDUPE_FILE = 'test/data/dupes-obvious.json');
+// }}}
+
+// compile - create binary files {{{
+var electronBuilder = (args, done) =>
+	spawn('node', ['./node_modules/electron-builder/out/cli/cli.js'].concat(args), {stdio: 'inherit'})
+		.on('close', ()=> done());
+
+
+// Compile the binaries
+gulp.task('compile', ['build'], done => electronBuilder(['-mwl'], done));
+gulp.task('compile:lin', ['build'], done => electronBuilder(['--linux', '--x64'], done));
+gulp.task('compile:mac', ['build'], done => electronBuilder(['--mac', '--x64'], done));
+gulp.task('compile:win', ['build'], done => electronBuilder(['--win', '--x64'], done));
+gulp.task('compile:win32', ['build'], done => electronBuilder(['--win', '--ia32'], done));
+// }}}
